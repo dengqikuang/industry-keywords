6ebb7eb1133b95b41cd384db72d8459dd85d0c51
DengQikuang <837935752@qq.com>
2026-05-06 21:25:59 +0800

    feat: add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { useState } from "react";
+import { IndustryKeywordApp } from "@/components/IndustryKeywordApp";
+
+export default function Home() {
+  return <IndustryKeywordApp />;
+}
